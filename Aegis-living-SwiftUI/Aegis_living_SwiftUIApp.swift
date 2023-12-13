fcefc1983e7de4444c4434a930432d0ffe0752d9
Febin Puthalath <febz@Febins-MacBook-Pro-2.local>
2023-12-13 18:18:51 +0530

    initial commit

---
+++ Aegis-living-SwiftUI/Aegis_living_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aegis_living_SwiftUIApp.swift
+//  Aegis-living-SwiftUI
+//
+//  Created by Febin Puthalath on 13/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Aegis_living_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
